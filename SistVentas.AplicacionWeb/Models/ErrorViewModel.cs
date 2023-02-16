31d42269589785fe5f306ff4c7caf147d460199a
mardc89 <mardc0602@gmail.com>
2023-02-16 13:17:00 -0500

    DataBase First

---
+++ SistVentas.AplicacionWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistVentas.AplicacionWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SistVentas.IOC/Dependencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistVentas.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace SistVentas.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services,IConfiguration Configuration){
+
+            services.AddDbContext<DBVENTASContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            });
+
+        }
+    }
+}
